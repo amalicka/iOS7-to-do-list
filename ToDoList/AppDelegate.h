61f67a2d22525448bd5830648f0db53a4289a72f
Ola Skierbiszewska <olaskierbiszewska@Olas-MacBook-Air.local>
2014-04-19 20:23:49 +0200

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Ola Skierbiszewska on 19/04/14.
+//  Copyright (c) 2014 Ola Skierbiszewska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
